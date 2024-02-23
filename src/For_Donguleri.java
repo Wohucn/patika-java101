5ab0a494fd5ccdfecebed4a39e45c5e101724c5f
Tayfun Sezen <tayfunsezen07@gmail.com>
2024-02-23 21:28:43 +0300

    do-while Döngüsü

---
+++ src/For_Donguleri.java
@@ -0,0 +1,15 @@
+public class For_Donguleri {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("-------------------");
+
+        int k = 1;
+        while (k <=10){
+            System.out.println(k);
+            k++;
+        }
+    }
+}
