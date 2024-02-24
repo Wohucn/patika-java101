62a02a9477c39ed040a79b76ac7893932c01663b
Tayfun Sezen <tayfunsezen07@gmail.com>
2024-02-24 16:11:58 +0300

    Contine_Break kısa bir örnek

---
+++ src/Contine_Break.java
@@ -0,0 +1,23 @@
+public class Contine_Break {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i > 5) {
+                break;
+            }
+            System.out.println(i);
+        }
+
+        System.out.println("-------------------------");
+
+        int a = 1;
+        while (a <= 10) {
+            a++;
+            if (a % 2 == 0) {
+
+                continue;
+            }
+            System.out.println(a);
+
+        }
+    }
+}
